b2ae740fc07323f55e8cce221b83b8ce1c29a529
Tomas Sidenfaden <trsidenfaden@yahoo.com>
2017-07-13 12:15:25 -0700

    Implement imagePicker delegate functions

--- MemeMe/MemeMe/ViewController.swift
+++ MemeMe/MemeMe/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var imagePickerView: UIImageView!
     
@@ -19,7 +19,20 @@ class ViewController: UIViewController {
 
     @IBAction func pickAnImage(_ sender: Any) {
         let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
         self.present(imagePicker, animated: true, completion: nil)
     }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        self.dismiss(animated: true, completion: nil)
+    }
 
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            imagePickerView.image = image
+        } else {
+            print("Image loading failed")
+        }
+        self.dismiss(animated: true, completion: nil)
+    }
 }
