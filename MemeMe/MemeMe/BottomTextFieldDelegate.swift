afb6d7164552065af16f3f447b872f1641da26dd
Tomas Sidenfaden <trsidenfaden@yahoo.com>
2017-07-13 16:17:38 -0700

    Add text field delegate swift files

---
+++ MemeMe/MemeMe/BottomTextFieldDelegate.swift
@@ -0,0 +1,11 @@
+//
+//  BottomTextFieldDelegate.swift
+//  MemeMe
+//
+//  Created by Tomas Sidenfaden on 7/13/17.
+//  Copyright © 2017 Tomas Sidenfaden. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
---
+++ MemeMe/MemeMe/TopTextFieldDelegate.swift
@@ -0,0 +1,10 @@
+//
+//  TopTextFieldDelegate.swift
+//  MemeMe
+//
+//  Created by Tomas Sidenfaden on 7/13/17.
+//  Copyright © 2017 Tomas Sidenfaden. All rights reserved.
+//
+
+import Foundation
+import UIKit
