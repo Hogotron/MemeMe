dc2b631b504bd45ea03860ee721756f1e84d187f
Tomas Sidenfaden <trsidenfaden@yahoo.com>
2017-07-17 12:39:54 -0700

    Implement textField functions in BottomTextFieldDelegate class

--- MemeMe/MemeMe/BottomTextFieldDelegate.swift
+++ MemeMe/MemeMe/BottomTextFieldDelegate.swift
@@ -11,9 +11,21 @@ import UIKit
 
 class BottomTextFieldDelegate: NSObject, UITextFieldDelegate {
     
+    // MARK: Text field delegate method
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        // Figure out what the new text will be if we return true
+        var newText = textField.text! as NSString
+        newText = newText.replacingCharacters(in: range, with: string) as NSString
+        
+        return true
+    }
+    
     func textFieldDidBeginEditing(_ textField: UITextField) {
-        if textField.text!.isEmpty {
+        if textField.text! == "BOTTOM" {
             textField.text = ""
+        } else {
+            textField.text! = textField.text!
         }
     }
 }
