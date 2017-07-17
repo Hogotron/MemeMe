0ac62d48a1e016f83d7d6312d69b502240690216
Tomas Sidenfaden <trsidenfaden@yahoo.com>
2017-07-17 15:35:40 -0700

    Add Meme struct file

---
+++ MemeMe/MemeMe/MemeStruct.swift
@@ -0,0 +1,14 @@
+//
+//  MemeStruct.swift
+//  MemeMe
+//
+//  Created by Tomas Sidenfaden on 7/17/17.
+//  Copyright © 2017 Tomas Sidenfaden. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Meme {
+    
+}
