4ec884766daa58a7da14516702f2eab440895013
Tomas Sidenfaden <trsidenfaden@yahoo.com>
2017-07-31 18:06:19 -0700

    Add CollectionViewController swift file

---
+++ MemeMe/MemeMe/CollectionViewController.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewController.swift
+//  MemeMe
+//
+//  Created by Tomas Sidenfaden on 7/31/17.
+//  Copyright © 2017 Tomas Sidenfaden. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewController: UICollectionViewController {
+    
+}
