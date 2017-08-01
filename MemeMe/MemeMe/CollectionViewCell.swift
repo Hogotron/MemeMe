84eac5ba84c4ecca9a33e988943c408adcf571df
Tomas Sidenfaden <trsidenfaden@yahoo.com>
2017-07-31 18:59:09 -0700

    Implement collection view controller cellForItemAt function

---
+++ MemeMe/MemeMe/CollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CollectionViewCell.swift
+//  MemeMe
+//
+//  Created by Tomas Sidenfaden on 7/31/17.
+//  Copyright © 2017 Tomas Sidenfaden. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var memeImageView: UIImageView!
+}
--- MemeMe/MemeMe/CollectionViewController.swift
+++ MemeMe/MemeMe/CollectionViewController.swift
@@ -10,4 +10,18 @@ import UIKit
 
 class CollectionViewController: UICollectionViewController {
     
+    let memes = [Meme]()
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "collectionViewCell", for: indexPath) as! CollectionViewCell
+        let meme = memes[indexPath.row]
+        
+        cell.memeImageView.image = meme.memedImage
+        
+        return cell
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return memes.count
+    }
 }
