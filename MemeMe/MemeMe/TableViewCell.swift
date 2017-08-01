d661a04a2de99632d257c7363bae835aa76123d3
Tomas Sidenfaden <trsidenfaden@yahoo.com>
2017-08-01 10:28:45 -0700

    Implement table view controller with custom cells

---
+++ MemeMe/MemeMe/TableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  TableViewCell.swift
+//  MemeMe
+//
+//  Created by Tomas Sidenfaden on 8/1/17.
+//  Copyright © 2017 Tomas Sidenfaden. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var memeImageView: UIImageView!
+    @IBOutlet weak var topTextLabel: UILabel!
+    @IBOutlet weak var bottomTextLabel: UILabel!
+    
+}
--- MemeMe/MemeMe/TableViewController.swift
+++ MemeMe/MemeMe/TableViewController.swift
@@ -10,4 +10,39 @@ import UIKit
 
 class TableViewController: UITableViewController {
     
+    var memes: [Meme]!
+    
+    @IBOutlet weak var cancelButton: UIBarButtonItem!
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        memes = appDelegate.memes
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TableViewCell", for: indexPath) as! TableViewCell
+        let meme = self.memes[indexPath.row]
+        
+        cell.memeImageView.image = meme.memedImage
+        cell.topTextLabel.text = meme.topText
+        cell.bottomTextLabel.text = meme.bottomText
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.memes.count
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let controller = self.storyboard?.instantiateViewController(withIdentifier: "ViewController") as! ViewController
+        controller.meme = [self.memes[indexPath.row]]
+        self.navigationController?.pushViewController(controller, animated: true)
+    }
+    
+    @IBAction func cancel(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
 }
